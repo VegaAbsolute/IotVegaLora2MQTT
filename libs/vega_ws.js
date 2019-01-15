4a3e76faf411e2b61d04f65de143cdf0a93e7bb0
alk120ger <alk120ger@yandex.ru>
2019-01-15 14:24:05 +0700

    update vega_ws
    Обновил модуль управления ws соединением

--- libs/vega_ws.js
+++ libs/vega_ws.js
@@ -1,5 +1,12 @@
-const WebSocket = require('ws');
-const EventEmitter = require('events');
+//vega_ws.ini version 1.0.2
+const DELAY_MESSAGE = 10000;
+const DELAY_RECONNECT = 5000;
+const MAX_DELAY_PING = 120000;
+
+let WebSocket = require( 'ws' );
+let EventEmitter = require( 'events' );
+let moment = require( 'moment' );
+
 class VegaWS extends EventEmitter
 {
   constructor(url)
@@ -7,95 +14,130 @@ class VegaWS extends EventEmitter
     super();
     this.url = url;
     this._connect = {
-      _status:false
+      _status: false,
+      _lastTimePing: undefined
     };
     this.reload();
-    const intervel = setInterval(()=>{
-      if(!this.status)
+    const intervel = setInterval( ()=>{
+      let currentDate = new Date().getTime();
+      let validlastTimePing = typeof this.lastTimePing == 'number';
+      if(!validlastTimePing) this.lastTimePing = currentDate;
+      let delayTimeMessage = currentDate - this.lastTimePing;;
+      if( !this.status || delayTimeMessage > MAX_DELAY_PING )
       {
-        var currentDate = new Date().getTime();
-        var validLastTimeReconnect = this.last_time_reconnect!==false;
-        var lastDate = validLastTimeReconnect?this.last_time_reconnect:currentDate;
-        var time = currentDate-lastDate;
-        if(time>10000)
+        let validLastTimeReconnect = this.last_time_reconnect !== false;
+        let lastDate = validLastTimeReconnect ? this.last_time_reconnect : currentDate;
+        let time = currentDate - lastDate;
+        if( time > DELAY_MESSAGE )
         {
           this.reload();
         }
       }
-    }, 5000);
+    }, DELAY_RECONNECT );
   }
-  get last_time_reconnect()
+  get last_time_reconnect ()
   {
-    var validLastTimeReconnect = this._connect._last_time_reconnect!==undefined&&typeof this._connect._last_time_reconnect==='number';
-    return validLastTimeReconnect?this._connect._last_time_reconnect:false;
+    let validLastTimeReconnect = this._connect._last_time_reconnect !== undefined && typeof this._connect._last_time_reconnect === 'number';
+    return validLastTimeReconnect ? this._connect._last_time_reconnect : false;
   }
-  set last_time_reconnect(ltr)
+  set last_time_reconnect ( ltr )
   {
     this._connect._last_time_reconnect = new Date().getTime();
   }
-  get status()
+  get status ()
   {
     return this._connect._status;
   }
-  set status(st)
+  get lastTimePing ()
+  {
+    return this._connect._lastTimePing;
+  }
+  set lastTimePing ( time )
+  {
+    this._connect._lastTimePing = time;
+  }
+  set status ( st )
   {
     this._connect._status = st;
   }
-  reload()
-  {
-    this._connect = new WebSocket(this.url);
-    this.status = false;
-    this._connect._self = this;
-    this.last_time_reconnect = new Date().getTime();
-    this._connect.on('open',this._open);
-    this._connect.on('message',this._message);
-    this._connect.on('error',this._error);
-    this._connect.on('close',this._close);
-  }
-  _message(message)
+  reload ()
   {
     try
     {
-      let obj = JSON.parse(message);
-      let validCMD = obj.cmd!=undefined;
-      if(validCMD)
-      {
-        this._self.emit(obj.cmd,obj)
-      }
+      if(typeof this._connect.terminate === 'function') this._connect.terminate();
+      this._connect = new WebSocket( this.url );
+      this.status = false;
+      this._connect._self = this;
+      this.last_time_reconnect = new Date().getTime();
+      this._connect.on('open', this._open);
+      this._connect.on('message', this._message);
+      this._connect.on('error', this._error);
+      this._connect.on('close', this._close);
+      this._connect.on('ping', this._ping);
     }
     catch (e)
     {
-      console.log(e);
+      console.log( moment().format('LLL'), e );
+    }
+  }
+  _message ( message )
+  {
+    try
+    {
+      let obj = JSON.parse( message );
+      let validCMD = obj.cmd != undefined;
+      if ( validCMD )
+      {
+        this._self.emit ( obj.cmd, obj )
+      }
+    }
+    catch ( e )
+    {
+      console.log( moment().format('LLL') + ': ', e );
     }
     finally
     {
       return;
     }
   }
-  _error()
+  _error ()
   {
-    console.log('WS error');
+    console.log( moment().format('LLL') + ': ', 'WS error');
     this._status = false;
-    this._self.emit('no_connect');
+    this._self.emit( 'no_connect' );
   }
-  _close(code)
+  _close ( code )
   {
-    console.log('WS close');
+    console.log( moment().format('LLL') + ': ', 'WS close' );
     this._status = false;
-    this._self.emit('no_connect');
+    this._self.emit( 'no_connect' );
   }
-  _open()
+  _open ()
   {
-    console.log('Successful connection on WS');
+    console.log( moment().format('LLL') + ': ', 'Successful connection on WS' );
     this._status = true;
-    this._self.emit('run')
+    this._self.emit( 'run' );
   }
-  send_json(obj)
+  _ping ()
   {
-    this._connect.send(JSON.stringify(obj),function(e){
-      if(e)
+    this._lastTimePing = new Date().getTime();
+    this._self.emit( 'ping' );
+  }
+  send_json ( obj )
+  {
+    let connect = this._connect;
+    this._connect.send( JSON.stringify( obj ), function( e ) {
+      if ( e )
       {
-        this._status=false;
+        console.log( moment().format('LLL'), e );
+        try
+        {
+          connect._status = false;
+        }
+        catch (e)
+        {
+          console.log( moment().format('LLL'), e );
+        }
       }
     });
   }
